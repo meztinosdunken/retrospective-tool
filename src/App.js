35c6b949581ea19beebeeddae014e94ee4047fdc
meztinosdunken <orlanmorry5462@hotmail.com>
2025-08-16 10:28:34 -0500

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Retrospective Tool</h1>
+      <p>Gather feedback and generate actionable insights.</p>
+    </div>
+  );
+};
+
+export default App;
